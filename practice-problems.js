cd8bd97164ad30420ab0c7e6eb761f75315b8569
Sawyer <sawyer.pearson92@gmail.com>
2022-02-02 16:32:37 -0700

    two-sum complete

--- practice-problems.js
+++ practice-problems.js
@@ -1,1 +1,19 @@
 // Here is a space to track all of your practice problems:
+
+// Two sum:
+// Given an array of integers (nums) & an integer (target), return the indices of the two nums that add up to the specified target
+
+var twoSum = function(nums, target) {
+    let solutions = [];
+    
+    for(let i=0; i < nums.length; i++){
+        for(let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] == target) {
+                solutions.push(i);
+                solutions.push(j);
+            }
+        }
+    }
+    return solutions;
+    
+};
